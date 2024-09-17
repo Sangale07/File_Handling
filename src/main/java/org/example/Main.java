a256d863cc75b91e257bf6a3be2ff3499bd82f7b
rushi <rushikeshsangale57@gmail.com>
2024-09-17 18:36:52 +0530

    Write File Method is Created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-    Main.getfileInfo();
+//    Main.getfileInfo();
+        Main.writeFile();
     }
     public static void getfileInfo(){
         File file =  new File("C:\\Users\\rushi\\Downloads\\firstProject\\File_handling\\test.txt");
@@ -20,6 +22,20 @@ public class Main {
         }
         else{
             System.out.println("File not Exist");
+        }
+
+    }
+    public static void writeFile(){
+        File file =  new File("C:\\Users\\rushi\\Downloads\\firstProject\\File_handling\\test.txt");
+        try{
+            FileWriter fr = new FileWriter(file);
+            String content = "Hii My Name is Rushikesh Sangale , I recently graduated from NMIET College in Computer Engineering";
+            fr.write(content);
+            fr.close();
+
+        }
+        catch(IOException e){
+           e.printStackTrace();
         }
     }
 
