9c4ee2910f9e5a229a69a6fac92dae468c26e788
rushi <rushikeshsangale57@gmail.com>
2024-09-17 18:28:03 +0530

    File Information method create

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,22 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-    File file = new File("C:\\Users\\rushi\\Downloads\\firstProject\\File_handling\\test.txt");
-    try{
-       if(file.createNewFile()){
-           System.out.println("Created new file:" + file.getName());
-       }
-       else{
-           System.out.println("file exist" + file.getName());
-       }
+    Main.getfileInfo();
     }
-    catch(IOException e){
-        System.out.println();
-        e.printStackTrace();
+    public static void getfileInfo(){
+        File file =  new File("C:\\Users\\rushi\\Downloads\\firstProject\\File_handling\\test.txt");
+        if(file.exists()){
+            System.out.println("File name :" + file.getName());
+            System.out.println("Readable :" + file.canRead());
+            System.out.println("Writable :" + file.canWrite());
+            System.out.println("Executable :" + file.canExecute());
+            System.out.println("Absolute Path :" + file.getAbsolutePath());
+            System.out.println("Path :" + file.getPath());
+            System.out.println("length" + file.length());
+        }
+        else{
+            System.out.println("File not Exist");
+        }
     }
-    }
+
 }
