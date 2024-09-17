bd2a6529a9ced9dd69d79703ecd12e96df9b1fb8
rushi <rushikeshsangale57@gmail.com>
2024-09-17 18:09:39 +0530

    File created successfully

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    File file = new File("C:\\Users\\rushi\\Downloads\\firstProject\\File_handling\\test.txt");
+    try{
+       if(file.createNewFile()){
+           System.out.println("Created new file:" + file.getName());
+       }
+       else{
+           System.out.println("file exist" + file.getName());
+       }
+    }
+    catch(IOException e){
+        System.out.println();
+        e.printStackTrace();
+    }
     }
 }
